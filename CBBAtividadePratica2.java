5e090b5c61fe8b2baa35cd76d3bfe64f10e30974
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-10-11 21:51:41 -0300

    pratica02

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,18 @@
+
+public class CBBAtividadePratica2 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int n1 = 8;
+        int n2 = 9;
+        int n3 = 10;
+        int soma = n1+n2+n3;
+        int resultado = soma/3;
+      System.out.println(n1);
+      System.out.println(n2);
+      System.out.println(n3);
+      System.out.println(soma);
+      System.out.println(resultado);
+}
+}
