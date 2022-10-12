13785dfe12d6c5e4bb2dfd8dcda26fb41eca292f
Isaelson Gutembergh <isaelsongutembergh@gmail.com>
2022-10-11 23:22:37 -0300

    Create BMQuestao04.java
    Adicionando BMQuestao04

---
+++ BMQuestao04.java
@@ -0,0 +1,19 @@
+public class BMQuestao04 {
+    public static void main(String[] args){
+        long long1= 100;
+        int int1 = 200;
+        int int2;
+       
+        int i = (int) long1;
+        int2 = int1 + i;
+       
+        System.out.println("O valor e "+ int2);
+        
+    
+
+        
+   
+    
+    }
+}
+// Da forma que se pede na questão, não irá rodar, pois não e permitido converter de long para int//
