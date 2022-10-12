6694e8c2d8a31e773aa9de31f56a1425d27f89ae
Isaelson Gutembergh <isaelsongutembergh@gmail.com>
2022-10-11 23:21:42 -0300

    Create BMQuestao03.java
    Adicionando BMQuestao04

---
+++ BMQuestao03.java
@@ -0,0 +1,19 @@
+public class BMQuestao03{
+    public static void main (String[] args) {
+        final float taxa = 8.25f;
+        float arroz;
+        float feijao;
+        float total;
+        float taxaCalculada;
+        arroz = 2.95f;
+        feijao = 3.50f;
+       
+
+        System.out.println("O item 1 custa " + arroz + " e o item 2 custa " + feijao);
+        total = arroz + feijao;
+        System.out.println("O total e "+ total);
+        taxaCalculada = total + 532125;
+
+    }
+    
+}
