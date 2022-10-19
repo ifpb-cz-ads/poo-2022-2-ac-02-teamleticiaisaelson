d57e8aaebd70bf02b34345ced97cdd072c3e1b10
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-10-19 08:47:02 -0300

    Update BMQuestao03.java

--- BMQuestao03.java
+++ BMQuestao03.java
@@ -1,19 +1,27 @@
 public class BMQuestao03{
     public static void main (String[] args) {
         final float taxa = 8.25f;
-        float arroz;
-        float feijao;
-        float total;
-        float taxaCalculada;
-        arroz = 2.95f;
-        feijao = 3.50f;
-       
-
+        /*letra a*/
+        double arroz = 2.95;
+        double feijao = 3.50;
+        /*letra b*/
         System.out.println("O item 1 custa " + arroz + " e o item 2 custa " + feijao);
-        total = arroz + feijao;
-        System.out.println("O total e "+ total);
-        taxaCalculada = total + 532125;
-
+        /*letra c*/
+        double total = arroz + feijao;
+        System.out.println("O total é:"+ total);
+        /*letra d*/
+        double taxa = 0.0825;
+        double taxaCalculada;
+        taxaCalculada = total * taxa;
+        System.out.println("O total da taxa calculada é:" + taxaCalculada);
+        /*letra e*/
+        arroz += taxaCalculada;
+        feijao += taxacalculada;
+        double novoCusto = arroz + feijao;
+        System.out.println("O custo de venda com a taxa é:" + novoCusto);
+        /*letra f*/
+        boolean muitoCaro = (novoCusto > 10);
+        System.out.println(muitCaro);
     }
     
 }
