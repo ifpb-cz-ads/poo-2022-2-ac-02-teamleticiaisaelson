88c2f1c322c2514369aeae121fab98cd4de51477
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-10-19 08:15:38 -0300

    Update CBBAtividadePratica3.java

--- CBBAtividadePratica3.java
+++ CBBAtividadePratica3.java
@@ -2,7 +2,6 @@ public class CBBAtividadePratica3 {
     public static void main(String[] args) {
         int base = 5;
         int altura = 10;
-        int area = base * altura;
-        System.out.println("A área do retângulo é:" + area);
+        System.out.println("A área do retângulo é:" + base * altura);
     }
 }
